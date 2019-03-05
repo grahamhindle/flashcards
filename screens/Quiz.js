3d9f67628a5d0197641a2e6e61a5414b736898b2
Graham Hindle <grahamhindle@mac.com>
2019-03-05 08:22:40 +0100

    added empty check on cards

--- screens/Quiz.js
+++ screens/Quiz.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {  Button } from 'react-native'
+
 import FlipCard from '../Components/FlipCard'
-import { View,Card, Container , Text,DeckSwiper} from 'native-base'
+import { View,Card, Container ,Button,Icon, Text,DeckSwiper} from 'native-base'
 import {connect} from 'react-redux'
 import {selectQuestions , selectDeck} from '../Selectors/questionSelector'
 
@@ -22,20 +22,36 @@ class Quiz extends React.Component {
 	render() {
 		
 		const {questionSet} = this.props
-		console.log('qset',questionSet)
+		const num = questionSet.length
 		return (
 				<Container>
 					<DeckSwiper
-						dataSource={questionSet}
+					ref={(c) => this._deckSwiper = c}
+					dataSource={questionSet}
+            renderEmpty={() =>
+              <View style={{ alignSelf: "center" }}>
+                <Text>Over</Text>
+              </View>
+						}
 						renderItem={item =>
 						<FlipCard key={item.id}
 						front={item.questionText}
 						back={item.answerText}>
+						number={questionSet.length}
 						</FlipCard>
-					
+						
 						}
 						/>
-						
+						<View style={{ flexDirection: "row", flex: 1, position: "absolute", bottom: 50, left: 0, right: 0, justifyContent: 'space-between', padding: 15 }}>
+          <Button iconLeft onPress={() => this._deckSwiper._root.swipeLeft()}>
+            <Icon name="arrow-back" />
+            <Text>Swipe Left</Text>
+          </Button>
+          <Button iconRight onPress={() => this._deckSwiper._root.swipeRight()}>
+            <Icon name="arrow-forward" />
+            <Text>Swipe Right</Text>
+          </Button>
+        </View>
 				</Container>
 		)
 	}  
@@ -45,6 +61,7 @@ const mapStateToProps = (state,ownProps)=> {
 	console.log('id', id)
 	return {
 		questionSet: selectQuestions(state,id)	
+
 	}
 }
 export default connect(
